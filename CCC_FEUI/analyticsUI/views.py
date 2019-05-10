bfbec97aed644e43879293430d254704de7f13fa
srijanc <srijan_am@yahoo.co.in>
2019-05-10 13:11:17 +1000

    Base UI Setup

--- CCC_FEUI/analyticsUI/views.py
+++ CCC_FEUI/analyticsUI/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import AnalyticsUI
+
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Analytics')
 
-    return render(request, 'analyticsUI/index.html', {
-        'title': 'Latest Analytics Views'
-    })
+    analyticsui = AnalyticsUI.objects.all()[:10]
+
+    context = {
+        'title': 'Latest Analytics Views',
+        'analyticsui': analyticsui
+    }
+
+    return render(request, 'analyticsUI/index.html', context)
+
+def details(request, id):
+    analyticsui = AnalyticsUI.objects.get(id=id)
+
+    context = {
+        'analyticsui': analyticsui
+    }
+
+    return render(request, 'analyticsUI/details.html', context)
