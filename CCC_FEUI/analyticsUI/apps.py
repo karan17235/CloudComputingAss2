8fe9bfe51c0fde8e86ad12f0d17887dad5ccc5ae
srijanc <srijan_am@yahoo.co.in>
2019-05-10 17:05:26 +1000

    Transformation of Old UI to New UI

--- CCC_FEUI/analyticsUI/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AnalyticsuiConfig(AppConfig):
-    name = 'analyticsUI'
--- CCC_FEUI/mapping/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Incidences(models.Model):
-    name = models.CharField(max_lenght=20)
-    location = models.CharField(max_length=20)
-
-    def __unicode__(self):
-        return self.name
