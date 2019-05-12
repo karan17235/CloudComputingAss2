75cd7b4c68575f50faa61fec4339572b6025b783
srijanc <srijan_am@yahoo.co.in>
2019-05-12 20:13:36 +1000

    Table Creation in Models

--- cloudUI/website/models.py
+++ cloudUI/website/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Trend(models.Models):
+    month_name = models.CharField(max_length=50)
+    month_data = models.IntegerField(max_length=10)
+
+class Profile(models.Model):
+    profile_public = models.IntegerField(max_length=20)
+    profile_media = models.IntegerField(max_length=20)
+    profile_politicians = models.IntegerField(max_length=20)
+
+class Content(models.Models):
+    group1_kw = models.IntegerField(max_length=20)
+    group2_kw = models.IntegerField(max_length=20)
+    group3_kw = models.IntegerField(max_length=20)
