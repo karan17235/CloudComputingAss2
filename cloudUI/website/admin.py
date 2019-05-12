92791c57a975ca6599623aff3449c9ee217567b9
srijanc <srijan_am@yahoo.co.in>
2019-05-13 04:24:34 +1000

    Graph done except content analysis

--- cloudUI/website/admin.py
+++ cloudUI/website/admin.py
@@ -1,14 +1,4 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-# from django.contrib import admin
-
-# # Importing from models.py
-# from .models import Trend
-# from .models import Profile
-# from .models import Content
-
-# # Register your models here.
-# admin.site.register(Trend)
-# admin.site.register(Profile)
-# admin.site.register(Content)
+from django.contrib import admin
--- cloudUI/website/views.py
+++ cloudUI/website/views.py
@@ -7,6 +7,8 @@ import time
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+user = "admin"
+password = "admin"
 #from .models import Profile, Trend, Content
 # Create your views here.
 
@@ -28,10 +30,6 @@ def about(request):
 
 def trend(request):
     trend_list = {}
-    #Trend.objects.filter(trend_name="trend").delete()
-    user = "admin"
-    password = "admin"
-    
     couchserver = couchdb.Server("http://%s:%s@172.26.38.45:5984/" % (user,password))
     db_name = {'adelaide': ['immigrant_adelaide', 'all_keywords_adelaide', 'immigrant_adelaide', 'profile_analysis','trend_analysis'], 'brisbane': ['immigrant_brisbane', 'all_keywords_brisbane', 'immigrant_brisbane', 'profile_analysis','trend_analysis'], 'canberra': ['immigrant_canberra', 'all_keywords_canberra', 'immigrant_canberra', 'profile_analysis','trend_analysis'], 'melbourne': ['immigrant_melbourne', 'all_keywords_melbourne', 'immigrant_melbourne', 'profile_analysis','trend_analysis'], 'perth': ['immigrant_design', 'all_keywords_perth', 'immigrant_view', 'profile_analysis_view','trend_analysis'], 'sydney': ['immigrant_sydney', 'all_keywords_sydney', 'immigrant_sydney', 'profile_analysis','trend_analysis']}
 
@@ -77,11 +75,32 @@ def trend(request):
     print(trend_list)
     return render(request, "website/trend.html", {'trend_list':trend_list})
 
-def scenario2(request):
-    return render(request, "website/about.html", {})
+def content(request):
+    #content_list = {}
+    return render(request, "website/content.html", {'content_list':content_list})
 
-def scenario3(request):
-    return render(request, "website/about.html", {})
+def profile(request):
+    profile_list = {}
+    trend_count = {"MEDIA": {"POSITIVE": 0, "NEGATIVE": 0,"NEUTRAL": 0},"POLITICS":{"POSITIVE": 0, "NEGAITVE": 0, "NEUTRAL": 0},"PUBLIC" : {"POSITIVE": 0,"NEGATIVE": 0, "NEUTRAL" : 0}}
+    total_count = {"MEDIA":0, "POLITICS":0,"PUBLIC":0}
+    couchserver = couchdb.Server("http://%s:%s@172.26.38.40:5984/" % (user,password))
+    db = couchserver["users"]
+    para = ('profile_analysis/profile')
+    flag = True
+    while flag:
+        try:
+            for item in db.view(para, group=True, group_level=2):
+                user_type = item.key[0].strip()
+                trend_count[user_type][item.key[1]] = item.value
+                total_count[user_type] += item.value
+                flag = False
+        except couchdb.http.ServerError:
+                print("Retrying")
+                time.sleep(3*60)
+    profile_list["sentiment"] = trend_count
+    profile_list["total"] = total_count
+
+    return render(request, "website/profile.html", {'profile_list':profile_list})
 
 def georep(request):
     return render(request, "website/georep.html", {})
