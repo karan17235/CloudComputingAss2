ed32cee5577c6e96dc78f3c70e32ee102a0b43fb
srijanc <srijan_am@yahoo.co.in>
2019-05-14 02:24:57 +1000

    Heat Map functionality added - Karan

--- cloudUI/website/views.py
+++ cloudUI/website/views.py
@@ -83,11 +83,8 @@ def content(request):
     g5 = ['affordable', 'racist', 'public', 'financial', 'insecure', 'migration rate', 'market', 'census', 'immigrate', 'immigration laws', 'influx of immigrants', 'first generation immigrant', 'flood of migrants', 'chain migrationg', 'voluntary migration', 'forced migration', 'migrant labor', 'emigration', 'permanent resident', 'temporary resident', 'undocumented', 'illegal immigrant', 'repatriation', 'resettlement', 'colonization', 'counter-urbanization', 'mobility', 'push factor', 'refugee status', 'refugee crisis', 'refugee claimant', 'resettled refugee', 'refugee camp', 'political refugee', 'economic refugee', 'refugee flow', 'violence', 'stateless person', 'flee', 'shelter', 'loss', 'mayhem', 'anguish', 'freedom', 'deterioration', 'border', 'local', 'smuggling', 'trafficker', 'coyote', 'detained', 'medical aid', 'deportation', 'brain drain', 'border patrol', 'sanctuary city', 'sanctuary cities', 'sanctuary church', 'fence', 'trump', 'border', 'airport', 'airports', 'customs', 'border', 'borders', 'resettlement', 'colonization', 'immigratiions']
 
     couchserver = couchdb.Server("http://%s:%s@172.26.38.45:5984/" % (user,password))
-
     db_name = {'adelaide': ['immigrant_adelaide', 'all_keywords_adelaide', 'immigrant_adelaide', 'profile_analysis','trend_analysis'], 'brisbane': ['immigrant_brisbane', 'all_keywords_brisbane', 'immigrant_brisbane', 'profile_analysis','trend_analysis'], 'canberra': ['immigrant_canberra', 'all_keywords_canberra', 'immigrant_canberra', 'profile_analysis','trend_analysis'], 'melbourne': ['immigrant_melbourne', 'all_keywords_melbourne', 'immigrant_melbourne', 'profile_analysis','trend_analysis'], 'perth': ['immigrant_design', 'all_keywords_perth', 'immigrant_view', 'profile_analysis_view','trend_analysis'], 'sydney': ['immigrant_sydney', 'all_keywords_sydney', 'immigrant_sydney', 'profile_analysis','trend_analysis']}
-
     city_count = {"adelaide" : {"g1":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g2":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g3":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g4":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g5":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0}}, "brisbane": {"g1":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g2":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g3":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g4":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g5":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0}}, "canberra" : {"g1":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g2":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g3":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g4":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g5":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0}}, "perth" : {"g1":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g2":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g3":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g4":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g5":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0}},"melbourne" : {"g1":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g2":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g3":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g4":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g5":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0}},"sydney" : {"g1":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g2":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g3":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g4":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g5":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0}}}
-
     total_count ={"g1":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g2":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g3":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g4":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0},"g5":{"POSITIVE":0,"NEGATIVE":0,"NEUTRAL":0}}
 
     content_list = {}
@@ -123,14 +120,51 @@ def content(request):
                 print("Retrying")
                 time.sleep(3*60)
 
+    # for keywords database
+        group_total = {'g1':0,'g2':0,'g3':0,'g4':0,'g5':0,}
+        db = couchserver['keywords']
+        print(db)
+        para = ('immigrant_keywords/all_keywords')
+        flag = True
+        while flag:
+            try:
+                for item in db.view(para, group = True, group_level=2):
+                    print(item.key[0],item.key[1],item.value)     
+                    if item.key[0] in g1:
+                        group_total['g1'] += item.value
+                    elif item.key[0] in g2:
+                        group_total['g2'] += item.value
+                    elif item.key[0] in g3:
+                        group_total['g3'] += item.value
+                    elif item.key[0] in g4:
+                        group_total['g4'] += item.value
+                    elif item.key[0] in g5:
+                        group_total['g5'] += item.value
+                flag = False
+            except couchdb.http.ServerError:
+                print("Retrying")
+                time.sleep(3*60)
+    city_group_total = {'adelaide':{},'brisbane':{},'canberra':{},'perth':{},'melbourne':{},'sydney':{}}
+    
+    for city,value in city_count.items():
+        for group,value_count in value.items():
+            final_count = 0
+            for sentiment,value in value_count.items():
+                final_count += value
+            group_total[group] += final_count
+            city_group_total[city][group] = final_count
+    content_list['total_group_count'] = group_total
+    content_list['city_group_total'] = city_group_total  
     content_list['city_count'] = city_count
     content_list['total_count'] = total_count
+
+    print(content_list)
     
     return render(request, "website/content.html", {'content_list':content_list})
 
 def profile(request):
     profile_list = {}
-    trend_count = {"MEDIA": {"POSITIVE": 0, "NEGATIVE": 0,"NEUTRAL": 0},"POLITICS":{"POSITIVE": 0, "NEGAITVE": 0, "NEUTRAL": 0},"PUBLIC" : {"POSITIVE": 0,"NEGATIVE": 0, "NEUTRAL" : 0}}
+    trend_count = {"MEDIA": {"POSITIVE": 0, "NEGATIVE": 0,"NEUTRAL": 0},"POLITICS":{"POSITIVE": 0, "NEGATIVE": 0, "NEUTRAL": 0},"PUBLIC" : {"POSITIVE": 0,"NEGATIVE": 0, "NEUTRAL" : 0}}
     total_count = {"MEDIA":0, "POLITICS":0,"PUBLIC":0}
     couchserver = couchdb.Server("http://%s:%s@172.26.38.45:5984/" % (user,password))
     db = couchserver["users"]
